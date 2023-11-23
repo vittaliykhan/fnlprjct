bc08a8a0b01a38e3d8b51b3c0929fd348658438c
vitaliykhan12 <vitaliykhan12@gmail.com>
2023-11-23 22:19:11 +0600

    Initial commit

---
+++ email.js
@@ -0,0 +1,23 @@
+function sendMail() {
+  var serviceId = "default_service";
+  var templateId = "bookclub";
+  var templateParams = {
+    from_name: $("#fullname").val(),
+    from_email: $("#email").val(),
+    title: $("#title").val(),
+    message: $("#description").val(),
+  };
+  emailjs
+    .send(serviceId, templateId, templateParams)
+
+    .then(
+      function (response) {
+        console.log("SUCCESS", response);
+        alert("Sent!");
+      },
+      function (error) {
+        console.log("FAILED", error);
+        alert("Send email failed!\r\n Response:\n " + JSON.stringify(error));
+      }
+    );
+}
